d759e37e6d96f2c5d984cc89c8fac8261c0e6470
chirag15898-lang <chirag15898@gmail.com>
2026-01-21 11:21:45 +0530

    Author Model class added

--- student-library-management-system/src/main/java/com/demo/example/student_library_management_system/model/Transaction.java
+++ student-library-management-system/src/main/java/com/demo/example/student_library_management_system/model/Transaction.java
@@ -1,4 +1,29 @@
 package com.demo.example.student_library_management_system.model;
 
+import com.demo.example.student_library_management_system.enums.TransactionType;
+import jakarta.persistence.*;
+
+import java.util.Date;
+@Entity
+@Table(name = "Transaction")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id" , nullable = false)
+    private int id;
+
+    @Column(name = "transaction_Type" , nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Column(name = "transaction_Date" , nullable = false)
+    private Date transactionDate;
+
+    @Column(name = "due_Date" , nullable = false)
+    private String dueDate;
+
+    @Column(name = "fine" , nullable = false)
+    private double fine;
+
 }
