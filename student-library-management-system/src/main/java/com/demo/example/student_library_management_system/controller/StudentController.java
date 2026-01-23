840e85e70da0d8890073a770740343d2de9feb42
chirag15898-lang <chirag15898@gmail.com>
2026-01-23 21:10:24 +0530

    Delete and Update operation added

--- student-library-management-system/src/main/java/com/demo/example/student_library_management_system/controller/StudentController.java
+++ student-library-management-system/src/main/java/com/demo/example/student_library_management_system/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.demo.example.student_library_management_system.controller;
 
+import com.demo.example.student_library_management_system.model.Student;
 import com.demo.example.student_library_management_system.requestdto.StudentRequestDto;
 
 import com.demo.example.student_library_management_system.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student/apis")
@@ -17,6 +20,34 @@ public class StudentController {
     public String saveStudent(@RequestBody StudentRequestDto studentRequestDto)
     {
       String response = studentService.saveStudent(studentRequestDto);
+      return response;
+    }
+
+    @GetMapping("/find{id}")
+    public Student findStudentById(@PathVariable int id)
+    {
+        Student student = studentService.getStudentById(id);
+        return student;
+    }
+
+    @GetMapping("/findAll")
+    public List<Student> findAllStudent()
+    {
+        List<Student> studentList = studentService.getAllStudent();
+        return studentList;
+    }
+
+    @PutMapping("update/{id}")
+    public String updateStudent(@PathVariable int id ,@RequestBody StudentRequestDto studentRequestDto)
+    {
+       String response = studentService.updateStudent(id,studentRequestDto);
+       return response;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudentById(@PathVariable int id)
+    {
+      String response = studentService.deleteStudentById(id);
       return response;
     }
 
--- student-library-management-system/src/main/java/com/demo/example/student_library_management_system/service/StudentService.java
+++ student-library-management-system/src/main/java/com/demo/example/student_library_management_system/service/StudentService.java
@@ -9,17 +9,19 @@ import com.demo.example.student_library_management_system.requestdto.StudentRequ
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
 
-    public String saveStudent(StudentRequestDto studentRequestDto)
-    {
+    public String saveStudent(StudentRequestDto studentRequestDto) {
         Student student = StudentConverter.convertStudentRequestDtoIntoStudent(studentRequestDto);
 
-        Card card= new Card();
+        Card card = new Card();
         card.setCardStatus(CardStatus.ACTIVE);
         card.setStudent(student);
 
@@ -28,4 +30,50 @@ public class StudentService {
         studentRepository.save(student);
         return "Student and card saved successfully";
     }
-}
+
+        public Student getStudentById(int id)
+        {
+            Optional<Student> studentOptional = studentRepository.findById(id);
+            if(studentOptional.isPresent())
+            {
+                return studentOptional.get();
+            }
+            return null;
+        }
+
+        public List<Student> getAllStudent()
+        {
+            List<Student> studentList = studentRepository.findAll();
+            return studentList;
+        }
+
+        public String updateStudent(int id,StudentRequestDto studentRequestDto)
+        {
+           Student student =  getStudentById(id);
+           if(student != null)
+           {
+               student.setAddress(studentRequestDto.getAddress());
+               student.setDepartment(studentRequestDto.getDepartment());
+               student.setDob(studentRequestDto.getDob());
+               student.setEmail(studentRequestDto.getEmail());
+               student.setGender(studentRequestDto.getGender());
+               student.setMobile(studentRequestDto.getMobile());
+               student.setName(studentRequestDto.getName());
+               student.setSemester(studentRequestDto.getSemester());
+
+               studentRepository.save(student);
+               return "Student Updated successfully";
+           }
+           else
+           {
+               return "Student not found cannot update";
+           }
+
+        }
+        public String deleteStudentById(int id)
+        {
+            studentRepository.deleteById(id);
+            return "student with id "+id+" deleted";
+        }
+    }
+
